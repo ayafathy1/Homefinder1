72d32ec91ef473e3cfd705ed29626278d544ed0d
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-01-30 21:50:29 +0200

    utilities

---
+++ homefinder1/lib/utilities/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+Color kPrimaryColor =Color(0xff6C63FF);
+Color kDarkBlueColor =Color(0xff0C243E);
+Color kGreyColor= Color(0xff858585);
+Color kLightGreyColor = Color(0xff858585).withOpacity(0.17);
