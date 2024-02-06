64151aaf1ce63d73a95cb216f9446380f485663b
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-02-06 18:16:23 +0200

    Update custom_bottom_navigation_bar_widget.dart

--- homefinder1/lib/Widget/custom_bottom_navigation_bar_widget.dart
+++ homefinder1/lib/Widget/custom_bottom_navigation_bar_widget.dart
@@ -1,42 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 class CustomBottomNavigationBarWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-    return  BottomNavigationBar(
-      backgroundColor: Colors.white10,
-      elevation: 0,
-      iconSize: 30,
-      unselectedItemColor: Color(0xff8694A6),
-      selectedItemColor: Color(0xff6C63FF),
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Image(image: AssetImage('lib/assets/images/HomeIcon.png')),
-          backgroundColor: Color(0xff),
-          label: "Home",
-
-        ),
-        BottomNavigationBarItem(
-            icon: Image(image: AssetImage('lib/assets/images/LocationIcon.png')),
-            backgroundColor: Color(0xff),
-            label: "Location"
-        ),
-        BottomNavigationBarItem(
-            icon: Image(image: AssetImage('lib/assets/images/SettingsIcon.png')),
-            backgroundColor: Color(0xff),
-            label: "Settings"
-        ),
-        BottomNavigationBarItem(
-            icon: Image(image: AssetImage('lib/assets/images/PersonIcon.png')),
-            backgroundColor: Color(0xff),
-            label: "Profile"
-        ),
-      ],);
-=======
     return  Container(
       height: 70,
       decoration: BoxDecoration(color: Colors.white,border: Border.all(color: Colors.white),
@@ -76,7 +42,7 @@ class CustomBottomNavigationBarWidget extends StatelessWidget{
           ),
         ],),
     );
->>>>>>> Stashed changes
+
   }
 
 }
