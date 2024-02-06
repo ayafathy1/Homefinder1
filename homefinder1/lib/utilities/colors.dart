c61bc8f2c26b12d95271ece51c7ab437c65017b8
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-02-06 17:55:38 +0200

    Update colors.dart

--- homefinder1/lib/utilities/colors.dart
+++ homefinder1/lib/utilities/colors.dart
@@ -4,3 +4,4 @@ Color kPrimaryColor =Color(0xff6C63FF);
 Color kDarkBlueColor =Color(0xff0C243E);
 Color kGreyColor= Color(0xff858585);
 Color kLightGreyColor = Color(0xff858585).withOpacity(0.17);
+Color kVeryLightGreyColor = Color(0xffF4F4F4);
