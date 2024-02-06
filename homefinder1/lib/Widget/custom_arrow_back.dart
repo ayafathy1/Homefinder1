9bd7e1eb605b275f77bce0951b7d289422d82fe6
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-02-06 18:54:07 +0200

    updates

--- homefinder1/lib/Widget/custom_arrow_back.dart
+++ homefinder1/lib/Widget/custom_arrow_back.dart
@@ -6,12 +6,20 @@ class CustomArrowBack extends StatelessWidget{
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SizedBox(width: 30,),
+        SizedBox(width: 25,),
         Container(
-          color: Color(0xffF4F4F4),
-          child: IconButton(onPressed: (){
-            Navigator.pop(context);
-          }, icon: Icon(Icons.arrow_back_ios ,color: Color(0xff6C63FF),) ,),
+          width: 50,
+          height: 55,
+          decoration: BoxDecoration(color: Color(0xff9292FD).withOpacity(0.2),
+              border: Border.all(color: Color(0xff9292FD).withOpacity(0.2)),borderRadius: BorderRadius.circular(15)),
+          child:
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: IconButton(onPressed: (){
+                  Navigator.pop(context);
+                }, icon: Icon(Icons.arrow_back_ios ,size: 20,color: Color(0xff6C63FF),) ,),
+              ),
+
         ),
       ],
     );
