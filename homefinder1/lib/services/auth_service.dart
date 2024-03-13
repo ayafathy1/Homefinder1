b0f08802831a98bc39a07683bf3e14604c412dc7
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2024-03-14 00:26:37 +0200

    add services

---
+++ homefinder1/lib/services/auth_service.dart
@@ -0,0 +1,16 @@
+import '../Utils/api_service.dart';
+import '../Utils/services.dart';
+
+class AuthServices{
+  static ApiService api = ApiService();
+  static signingIn(String email,String password) async {
+    var data = await api.request(Services.signingInEndPoint, "POST",queryParamters: {
+      "email":email,
+      "password":password,
+    });
+    if (data != null) {
+
+    }
+    return null;
+  }
+}
