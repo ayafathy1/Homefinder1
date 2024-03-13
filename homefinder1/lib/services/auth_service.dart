b06fb413ba8e7be9ee5fc9c4867f4e7b93b570c2
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2024-03-14 00:27:50 +0200

    Update auth_service.dart

--- homefinder1/lib/services/auth_service.dart
+++ homefinder1/lib/services/auth_service.dart
@@ -4,7 +4,7 @@ import '../Utils/services.dart';
 class AuthServices{
   static ApiService api = ApiService();
   static signingIn(String email,String password) async {
-    var data = await api.request(Services.signingInEndPoint, "POST",queryParamters: {
+    var data = await api.request(Services.signingInEndPoint, "POST",data: {
       "email":email,
       "password":password,
     });
