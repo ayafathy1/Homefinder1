41c67e9fc38250d2fbc3402405a440b946b39a8d
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-04-28 18:21:15 +0300

    create delete account screen

---
+++ homefinder1/lib/Screens/delete_account/controller/delete_account_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class DeleteAccountController extends GetxController{
+  void onInit() {
+    super.onInit();
+    passwordVisible = false;
+  }
+  bool passwordVisible = false;
+
+
+  final  formkey =  GlobalKey<FormState>();
+  final passwordController =  TextEditingController();
+  String? passwordValidator(password) {
+    if (RegExp(
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+        .hasMatch(password!)) {
+
+    } else {
+      return "   Enter Correct password";
+    }
+  }
+}
