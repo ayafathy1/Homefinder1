d16588a719361ff5e5a40b297a3edd6121715ff6
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-05-16 22:55:51 +0300

    create add listing third details screen

---
+++ homefinder1/lib/Screens/add_listing_third_deatials/add_listing_3rd_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddListingThirdDetailsScreen extends StatelessWidget {
+  const AddListingThirdDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
